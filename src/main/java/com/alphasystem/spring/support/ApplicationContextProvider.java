db9c4c52f875a3b08038e6ab2534b2f8594e98a8
sfali23 <f.syed.ali@gmail.com>
2017-05-31 08:34:11 -0400

    App level "ApplicationContextAware" implementation

---
+++ src/main/java/com/alphasystem/spring/support/ApplicationContextProvider.java
@@ -0,0 +1,24 @@
+package com.alphasystem.spring.support;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author sali
+ */
+@Component
+public class ApplicationContextProvider implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    public static <T> T getBean(Class<T> type) {
+        return applicationContext.getBean(type);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ApplicationContextProvider.applicationContext = applicationContext;
+    }
+}
